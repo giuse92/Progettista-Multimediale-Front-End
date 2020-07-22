af2f7a64ebd1d148df2e91e0a0714f4f8a132a74
Giuseppe <giuseppe.ilsami@gmail.com>
2020-07-22 18:03:24 +0200

    esercizio 22-7 pagina studenti, fatti cicli for e array di oggetti studenti

---
+++ js/fundamentals_3/esercizio-22-7/main.js
@@ -0,0 +1,72 @@
+/*
+Pagina web che visualizzi le informazioni relative a N studenti.
+Un minimo di 5 studenti.
+Ogni studente deve essere descritto, al minimo, dalle proprietà:
+matricola, nome, cognome.
+Gli oggetti che userete per modellare gli studenti devono
+NECESSARIAMENTE essere inseriti e gestiti da un Array.
+Iterando sull'array (sia con for che con for of), dovete visitare ogni studente e visualizzarne
+le prorietà (i valori)
+Il tutto dentro un div getElementByid();*/
+
+let divStudenti = document.getElementById('mostraStudenti');
+let studenti = [
+    {
+        nMatricola: 1,
+        nome: "Bentley",
+        cognome: "Marquez"
+    },
+    {
+        nMatricola: 2,
+        nome: "Gabija",
+        cognome: "Randolph"
+    },
+    {
+        nMatricola: 3,
+        nome: "Caoimhe",
+        cognome: "Vaughan"
+    },
+    {
+        nMatricola: 4,
+        nome: "Samah",
+        cognome: "Mcdowell"
+    },
+    {
+        nMatricola: 5,
+        nome: "Kurtis",
+        cognome: "Brett"
+    }
+];
+
+studenti[studenti.length] = {
+    nMatricola: studenti.length + 1,
+    nome: "Robbie",
+    cognome: "Hooper"
+}
+
+studenti.push({
+    nMatricola: studenti.length + 1,
+    nome: "Nikola",
+    cognome: "Fitzpatrick"
+});
+
+console.log(studenti)
+
+//Ciclo for semplice su array
+/*for (let i = 0; i < studenti.length; i++) {
+    let objStudente = studenti[i]
+    //console.log(objStudente);
+    for (prop in objStudente) {
+        //console.log(objStudente[prop])
+        divStudenti.innerHTML += objStudente[prop] + "<br>";
+    };
+};*/
+
+//Ciclo for of per array, ciclo for in per oggetti
+for (objStudente of studenti) {
+    console.log(objStudente);
+    for (prop in objStudente) {
+        //console.log(objStudente[prop])
+        divStudenti.innerHTML += objStudente[prop] + "<br>";
+    };
+};
