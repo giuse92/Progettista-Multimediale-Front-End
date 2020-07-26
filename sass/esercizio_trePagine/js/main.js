6fc76b5a3616fa571b228207be4a6295378f9fb7
Giuseppe <giuseppe.ilsami@gmail.com>
2020-07-26 10:58:18 +0200

    finito main.js

--- sass/esercizio_trePagine/js/main.js
+++ sass/esercizio_trePagine/js/main.js
@@ -4,63 +4,130 @@ containerCinqueNemici = document.getElementById('cinqueNemici'),
 containerNemiciPotentissimi = document.getElementById('nemiciPotentissimi');
 
 //Array 10 nemici
-let nemici = [];
+let dieciNemici = [];
 
-nemici[nemici.length] = {
+dieciNemici[dieciNemici.length] = {
     "Nome e cognome": "Jack Napier",
     "Conosciuto/a come": "Joker" ,
-    "Descrizione": "Lorem ipsum"
+    "Descrizione": "Lorem ipsum",
+    tipo: "Boss",
+    haAbilitàSpeciali: true
 }
 
-nemici[nemici.length] = {
+dieciNemici[dieciNemici.length] = {
     "Nome e cognome": "Harleen Quinzel",
     "Conosciuto/a come": "Harley Quinn",
-    "Descrizione": "Lorem ipsum"
+    "Descrizione": "Lorem ipsum",
+    tipo: "Boss",
+    haAbilitàSpeciali: true
 }
 
-nemici[nemici.length] = {
+dieciNemici[dieciNemici.length] = {
     "Nome e cognome": "Jonathan Crane",
     "Conosciuto/a come": "Spaventapasseri",
-    "Descrizione": "Lorem ipsum"
+    "Descrizione": "Lorem ipsum",
+    tipo: "Boss",
+    haAbilitàSpeciali: true
 }
 
-nemici.push({
+dieciNemici.push({
     "Nome e cognome": "Harvey Dent",
     "Conosciuto/a come": "Due Facce",
-    "Descrizione": "Lorem ipsum"
+    "Descrizione": "Lorem ipsum",
+    tipo: "Boss",
+    haAbilitàSpeciali: true
 })
 
-nemici.push({
+dieciNemici.push({
     "Nome e cognome": "Oswald Cobblepot",
     "Conosciuto/a come": "Pinguino",
-    "Descrizione": "Lorem ipsum"
+    "Descrizione": "Lorem ipsum",
+    tipo: "Boss",
+    haAbilitàSpeciali: true
 })
 
-nemici.push({
+dieciNemici.push({
     "Nome e cognome": "Edward Nigma",
     "Conosciuto/a come": "Enigmista",
-    "Descrizione": "Lorem ipsum"
+    "Descrizione": "Lorem ipsum",
+    tipo: "Boss",
+    haAbilitàSpeciali: true
 })
 
-nemici.splice(3, 0,
+dieciNemici.splice(3, 0,
     {
-        "Nome e cognome": "Waylon Jones",
-        "Conosciuto/a come": "Killer Croc",
-        "Descrizione": "Lorem ipsum"
+        "Nome": "Tirapiedi di Due Facce",
+        "Descrizione": "Riconoscibili dall'abbigliamento. Alcuni indossano anche delle maschere.",
+        tipo: "NPC",
+        haAbilitàSpeciali: false
     },
     {
-        "Nome e cognome": "Jervis Tetch",
-        "Conosciuto/a come": "Cappellaio Matto",
-        "Descrizione": "Lorem ipsum"
+        "Nome": "Tirapiedi di Pinguino",
+        "Descrizione": "Riconoscibili dal pinguino disegnato dietro la loro giacca.",
+        tipo: "NPC",
+        haAbilitàSpeciali: false
     },
     {
-        "Nome e cognome": "Lazlo Valentin",
-        "Conosciuto/a come": "Professor Pyg",
-        "Descrizione": "Lorem ipsum"
+        "Nome": "Tirapiedi di Harley Quinn",
+        "Descrizione": "Sono vestiti e truccati da mimi.",
+        tipo: "NPC",
+        haAbilitàSpeciali: false
     },
     {
-        "Nome e cognome": "Garfield Lynns",
-        "Conosciuto/a come": "Firefly",
-        "Descrizione": "Lorem ipsum"
+        "Nome": "Informatori dell'Enigmista",
+        "Descrizione": "Miliziani, scagnozzi di altri supercriminali e poliziotti corrotti che lavorano segretamente per Nigma.",
+        tipo: "NPC",
+        haAbilitàSpeciali: false
     }
 )
+
+//ciclo for for nemici per stamparli nel div dieciNemici
+for (obj10Nem of dieciNemici) {
+    for (prop1 in obj10Nem) {
+        containerDieciNemici.innerHTML += `<strong style="background-color: #ccc">${prop1}</strong>: ${obj10Nem[prop1]}<br>`;
+    }
+}
+
+//Array 5 nemici presi da array dieciNemici
+let cinqueNemici = dieciNemici.slice(2, 7);
+
+//Output slice in contenitore cinqueNemici
+for (obj5Nem of cinqueNemici) {
+    for (prop2 in obj5Nem) {
+        containerCinqueNemici.innerHTML += `<strong style="background-color: yellow">${prop2}</strong>: ${obj5Nem[prop2]}<br>`;
+    }
+}
+
+//Array altri nemici potentissimi
+let altriNemiciBoss = [
+    {
+        "Nome": "Adamska",
+        "Conosciuto/a come": "Ocelot",
+        "Descrizione": "Lorem ipsum",
+        tipo: "Boss",
+        haAbilitàSpeciali: true
+    },
+    {
+        "Nome": "Eli",
+        "Conosciuto/a come": "Liquid Snake",
+        "Descrizione": "Lorem ipsum",
+        tipo: "Boss",
+        haAbilitàSpeciali: true
+    },
+    {
+        "Nome": "George Sears",
+        "Conosciuto/a come": "Solidus Snake",
+        "Descrizione": "Lorem ipsum",
+        tipo: "Boss",
+        haAbilitàSpeciali: true
+    },
+];
+
+//Array concatenazione dieciNemici e altriNemiciBoss con output
+let nemiciPotentissimi = dieciNemici.concat(altriNemiciBoss);
+
+for (objNemPot of nemiciPotentissimi) {
+    for (prop3 in objNemPot) {
+        containerNemiciPotentissimi.innerHTML += `<strong style="background-color: red">${prop3}</strong>: ${objNemPot[prop3]}<br>`;
+    }
+}
