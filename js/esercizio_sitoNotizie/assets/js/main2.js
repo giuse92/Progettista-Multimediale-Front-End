a80f9df17e995d33f70ee9455c72d9010b08b73c
Giuseppe <giuseppe.ilsami@gmail.com>
2020-08-15 20:50:02 +0200

    finita seconda versione esercizio_sitoNotizie

--- js/esercizio_sitoNotizie/assets/js/main2.js
+++ js/esercizio_sitoNotizie/assets/js/main2.js
@@ -146,6 +146,50 @@ let tutteLeNotizie = [
         a augue scelerisque malesuada. Vivamus commodo volutpat sem, sed accumsan nisl efficitur et. Sed laoreet aliquam ligula,
         nec blandit eros pretium at. Proin id fringilla leo.</p>`
     },
+    {
+        categoria: "sport",
+        titolo: "Titolo 3 sport",
+        paragrafi: `<p>Lorem 3 sport ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac tortor erat. Nunc et venenatis orci, id gravida
+        urna. Donec bibendum tempus vehicula. Mauris vestibulum nisi sit amet nulla varius, vitae mattis diam suscipit. Nulla
+        dictum massa suscipit, vehicula dui nec, euismod augue. Orci varius natoque penatibus et magnis dis parturient montes,
+        nascetur ridiculus mus. Curabitur lobortis pellentesque ultricies. Suspendisse ut egestas libero, sit amet vehicula
+        orci.</p>
+        <p>Duis ultrices, lectus a porttitor tincidunt, ipsum mauris vehicula augue, vitae vestibulum leo ipsum sit amet velit.
+        Nulla ornare dolor ac quam commodo, sed fringilla quam sollicitudin. In nec sollicitudin erat. Mauris pretium in turpis
+        at pretium. Sed id tortor sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+        Donec euismod quam at velit maximus, et rhoncus turpis elementum. Ut sit amet felis sollicitudin, pulvinar odio non,
+        suscipit nibh. Proin quis augue imperdiet, malesuada turpis et, dignissim dui. Quisque varius id ex id pretium.
+        Pellentesque laoreet, justo et cursus commodo, sapien nulla aliquam nunc, pulvinar congue ante sem ut tellus. Praesent
+        sed finibus eros. Maecenas malesuada cursus lacus eu efficitur. Nam non turpis nec purus vehicula eleifend. Nam
+        facilisis, ante in dapibus auctor, tortor enim lacinia ipsum, sed dapibus massa felis a neque. Proin facilisis nisl
+        arcu.</p>
+        <p>Etiam pretium magna non sodales rutrum. Cras a tortor vel sem molestie efficitur a vel dui. Aliquam sit amet interdum
+        lectus, a vestibulum libero. Vivamus id fermentum nisl. Quisque fermentum sit amet libero vitae mollis. Vivamus non est
+        a augue scelerisque malesuada. Vivamus commodo volutpat sem, sed accumsan nisl efficitur et. Sed laoreet aliquam ligula,
+        nec blandit eros pretium at. Proin id fringilla leo.</p>`
+    },
+    {
+        categoria: "gossip",
+        titolo: "Titolo 2 gossip",
+        paragrafi: `<p>Lorem 2 gossip ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac tortor erat. Nunc et venenatis orci, id gravida
+        urna. Donec bibendum tempus vehicula. Mauris vestibulum nisi sit amet nulla varius, vitae mattis diam suscipit. Nulla
+        dictum massa suscipit, vehicula dui nec, euismod augue. Orci varius natoque penatibus et magnis dis parturient montes,
+        nascetur ridiculus mus. Curabitur lobortis pellentesque ultricies. Suspendisse ut egestas libero, sit amet vehicula
+        orci.</p>
+        <p>Duis ultrices, lectus a porttitor tincidunt, ipsum mauris vehicula augue, vitae vestibulum leo ipsum sit amet velit.
+        Nulla ornare dolor ac quam commodo, sed fringilla quam sollicitudin. In nec sollicitudin erat. Mauris pretium in turpis
+        at pretium. Sed id tortor sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+        Donec euismod quam at velit maximus, et rhoncus turpis elementum. Ut sit amet felis sollicitudin, pulvinar odio non,
+        suscipit nibh. Proin quis augue imperdiet, malesuada turpis et, dignissim dui. Quisque varius id ex id pretium.
+        Pellentesque laoreet, justo et cursus commodo, sapien nulla aliquam nunc, pulvinar congue ante sem ut tellus. Praesent
+        sed finibus eros. Maecenas malesuada cursus lacus eu efficitur. Nam non turpis nec purus vehicula eleifend. Nam
+        facilisis, ante in dapibus auctor, tortor enim lacinia ipsum, sed dapibus massa felis a neque. Proin facilisis nisl
+        arcu.</p>
+        <p>Etiam pretium magna non sodales rutrum. Cras a tortor vel sem molestie efficitur a vel dui. Aliquam sit amet interdum
+        lectus, a vestibulum libero. Vivamus id fermentum nisl. Quisque fermentum sit amet libero vitae mollis. Vivamus non est
+        a augue scelerisque malesuada. Vivamus commodo volutpat sem, sed accumsan nisl efficitur et. Sed laoreet aliquam ligula,
+        nec blandit eros pretium at. Proin id fringilla leo.</p>`
+    }
 ]
 
 //Ciclo for per mostrare tutte le notizie di default
@@ -165,6 +209,7 @@ for (let i = 0; i < tutteLeNotizie.length; i++) {
     divArticoli.innerHTML += line;
 }
 
+//Funzione applicata ai button per filtrare/mostra tutte le notizie
 function filtraNews(strIndicatore) {
     if (strIndicatore == "tutteLeNotizie") {
         divArticoli.innerHTML = "";
@@ -200,14 +245,35 @@ function filtraNews(strIndicatore) {
             }
             line += `</div>`;
             divArticoli.innerHTML += line;
+            //Animazione applicata alla sezione **Notizie dall'estero**
+            animSez('.articoli .notizie-estero');
         }
     }
 }
 
-//Creo i button per filtrare le notizie
+//Creo i button in html per filtrare le notizie
 let classFiltro = sectionMostraNews.getElementsByClassName('filtro')[0];
 classFiltro.innerHTML += '<button onclick="filtraNews(\'tutteLeNotizie\')" class="filtra-news">Tutte le notizie</button>';
 classFiltro.innerHTML += '<button onclick="filtraNews(\'cronaca-nera\')" class="filtra-news">Cronaca nera</button>';
 classFiltro.innerHTML += '<button onclick="filtraNews(\'sport\')" class="filtra-news">Sport</button>';
 classFiltro.innerHTML += '<button onclick="filtraNews(\'notizie-estero\')" class="filtra-news">Notizie dall\' estero</button>';
 classFiltro.innerHTML += '<button onclick="filtraNews(\'gossip\')" class="filtra-news">Gossip</button>';
+
+//Function animazione che richiamo in filtraNews() sopra
+function animSez(getDiv) {
+    let start = 0;
+    let idInterval = null
+    let selectedDivs = sectionMostraNews.querySelectorAll(getDiv);
+    for (objDiv of selectedDivs) {
+        objDiv.hidden = true;
+    };
+    function showDiv() {
+        if (start < selectedDivs.length) {
+            selectedDivs[start].hidden = false;
+            start++;
+        } else {
+            clearInterval(idInterval);
+        }
+    }
+    idInterval = setInterval(showDiv, 1000);
+}
