6ca383b8a387ca91d34397bedfe3165a8836bebc
Giuseppe <giuseppe.ilsami@gmail.com>
2020-09-05 18:41:29 +0200

    prova temporizzazione semaforo.js

--- js/esercizio_manipolazioneCss/semaforo.js
+++ js/esercizio_manipolazioneCss/semaforo.js
@@ -26,3 +26,34 @@ function vertOrizz() {
         btnJs.innerText = "Premi per Semaforo Orizzontale";
     }
 }
+
+let btnAnimSemaforo = document.createElement('button');
+btnAnimSemaforo.innerHTML = "Fai partire animSemaforo";
+btnAnimSemaforo.setAttribute("onclick", "animSemaforo()");
+btnJs.after(btnAnimSemaforo);
+
+function animSemaforo() {
+    for (let i = 0; i < cerchiSemaforo.length; i++) {
+        cerchiSemaforo[i].style.pointerEvents = "none";
+    };
+    luceOffOn(cerchiSemaforo[0], cssClasses[0], 2000);
+    temporizza(cerchiSemaforo[2], cssClasses[2], 2000);
+    temporizza(cerchiSemaforo[1], cssClasses[1], 4000);
+    temporizza(cerchiSemaforo[0], cssClasses[0], 6000);
+}
+
+function luceOffOn(nLuce, strClasse, tmp) {
+    nLuce.classList.toggle(strClasse);
+    setTimeout(function () { 
+        nLuce.classList.toggle(strClasse);
+    }, tmp);
+};
+
+function temporizza(nLuce, strClasse, tmp) {
+    setTimeout(function () {
+        nLuce.classList.toggle(strClasse);
+        setTimeout(function () {
+            nLuce.classList.toggle(strClasse);
+        }, 2000);
+    }, tmp)
+};
