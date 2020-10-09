6b407d40278f90c8ea65192815948a23c8aee272
Giuseppe <giuseppe.ilsami@gmail.com>
2020-10-09 13:01:06 +0200

    v-bind, v-for, v-if else if else, v-on:click

--- vue/esempio_9Ott/vue_ex_0a.js
+++ vue/esempio_9Ott/vue_ex_0a.js
@@ -1,28 +1,66 @@
-var app = new Vue({
+let app = new Vue({
     el: "#app-1",
     data: {
         content: "Hello world"
     }
 });
 
-/*var app2 = new Vue({
+/*let app2 = new Vue({
     el: "#app-2",
     data: {
         messaggio: `Rand ${new Date().toLocaleString()}`
     }
 });*/
 
-var app2 = new Vue({
+let app2 = new Vue({
     el: "#app-2",
     data: {
         message: `Rand ${new Date().toLocaleString()}`
     }
 });
 
-var app3 = new Vue({
-    el: "app-3",
+let app3 = new Vue({
+    el: "#app-3",
     data: {
-        vedi: true
+        vedi: true, //o false
+        testo: 'Questo è un testo'
     }
-})
+});
 
+let app4 = new Vue({
+    el: "#app-4",
+    data: {
+        elements: [
+            { text: 'Javascript' },
+            { text: 'Vue' },
+            { text: 'C#' },
+            { text: 'Angular' }
+        ]
+    }
+});
+
+let app4a = new Vue({
+    el: "#app-4a",
+    data: {
+        elements: ['Javascript', 'Vue', 'C#', 'Angular']
+    }
+});
+
+let app5 = new Vue({
+    el: "#app-5",
+    data: {
+        orario: new Date().getHours()
+    }
+});
+
+let app6 = new Vue({
+    el: "#app-6",
+    data: {
+        message: 'ciao pippo tutto appo'
+    },
+    methods: {
+        invertiMessage: function() {
+            this.message = this.message.split('').reverse('').join('')
+        }
+    }
+});
---
+++ vue/esempio_9Ott/vue_ex_1a.js
@@ -0,0 +1,13 @@
+let app1 = new Vue({
+    el: "#msg",
+    data: {
+        messaggio: "Ciao a tutti"
+    }
+})
+
+let app2 = new Vue({
+    el: "#condizione-if",
+    data: {
+        testoColorato: true
+    }
+})
