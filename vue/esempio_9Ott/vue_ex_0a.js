e0a7057b03d7e48910cf0e709008b40d23088c6a
Giuseppe <giuseppe.ilsami@gmail.com>
2020-10-09 11:13:52 +0200

    v-bind e v-if

---
+++ vue/esempio_9Ott/vue_ex_0a.js
@@ -0,0 +1,28 @@
+var app = new Vue({
+    el: "#app-1",
+    data: {
+        content: "Hello world"
+    }
+});
+
+/*var app2 = new Vue({
+    el: "#app-2",
+    data: {
+        messaggio: `Rand ${new Date().toLocaleString()}`
+    }
+});*/
+
+var app2 = new Vue({
+    el: "#app-2",
+    data: {
+        message: `Rand ${new Date().toLocaleString()}`
+    }
+});
+
+var app3 = new Vue({
+    el: "app-3",
+    data: {
+        vedi: true
+    }
+})
+
