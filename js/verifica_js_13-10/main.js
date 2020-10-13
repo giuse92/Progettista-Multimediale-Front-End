da030976f22ee9ef2c6349868e4fb7f52d8ddc10
Giuseppe <giuseppe.ilsami@gmail.com>
2020-10-13 10:08:37 +0200

    celle tabella stilizzate

--- js/verifica_js_13-10/main.js
+++ js/verifica_js_13-10/main.js
@@ -1,19 +1,21 @@
 //Gestisco la promise
 let urlJson = 'https://my-json-server.typicode.com/typicode/demo/db';
-let mainS = document.querySelector('script');
 
 fetch(urlJson)
 .then(risposta => {
     if (risposta.ok) {
-        console.log(risposta.status); //se dà 200, è ok
+        console.log(risposta.status); // se status = 200, è ok 
         return risposta.json()
     }
 })
 .then(mioJson => {
+    let mainS = document.querySelector('script');
+    
     let costruisciTabella = (jsonArray) => {
         //jsonArray parametrizza l'array del json che andrò a ciclare nella funzione
         //richiamata due volte più giù
         let tabella = document.createElement('table');
+        tabella.setAttribute('style', 'text-align:center; width: 400px; border: 1px solid black')
         mainS.insertAdjacentElement('beforebegin', tabella);
         let rowTitoli = document.createElement('tr');
         tabella.prepend(rowTitoli);
@@ -25,11 +27,13 @@ fetch(urlJson)
 
             for (let prop in objArray) {
                 let tds = document.createElement('td');
+                tds.style.border = "1px solid black"
                 tds.innerHTML = objArray[prop];
                 trs.append(tds);
 
                 if(isFirstTime) {
                     let ths = document.createElement('th');
+                    ths.style.border = "1px solid blue"
                     ths.innerHTML = prop;
                     rowTitoli.append(ths);
                 }
